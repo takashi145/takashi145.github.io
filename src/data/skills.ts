929f546ea9a24d1881bd69842b5d4158cc9ae6c3
takashi145 <t.shuhei0203@gmail.com>
2026-09-19 23:25:11 +0900

    スキルのタグ修正

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,7 +1,6 @@
 export type Skill = {
   name: string;
-  use: string;
-  years: string;
+  tags?: string[];
   desc?: string;
 };
 
@@ -14,26 +13,26 @@ export const skillGroups: SkillGroup[] = [
   {
     group: '言語',
     items: [
-      { name: 'HTML5 / CSS3', use: '実務・趣味', years: '約4年' },
-      { name: 'JavaScript / Typescript', use: '実務・趣味', years: '約3年' },
-      { name: 'PHP', use: '趣味', years: '約4年' },
-      { name: 'SQL', use: '実務', years: '約3年' },
+      { name: 'HTML5 / CSS3' },
+      { name: 'JavaScript / TypeScript' },
+      { name: 'PHP' },
+      { name: 'SQL' },
     ],
   },
   {
     group: 'フレームワーク / ライブラリ',
     items: [
-      { name: 'Laravel', use: '実務・趣味', years: '約2年' },
-      { name: 'React', use: '実務・趣味', years: '約4年' },
-      { name: 'Vue.js', use: '趣味', years: '約2年' },
+      { name: 'Laravel' },
+      { name: 'React' },
+      { name: 'Vue.js' },
     ],
   },
   {
     group: 'ツール / インフラ',
     items: [
-      { name: 'Git / GitHub / GitLab', use: '実務・趣味', years: '約4年' },
-      { name: 'MySQL', use: '実務・趣味', years: '約2年' },
-      { name: 'Docker', use: '実務', years: '約1年' },
+      { name: 'Git / GitHub / GitLab' },
+      { name: 'MySQL' },
+      { name: 'Docker' },
     ],
   },
 ];
